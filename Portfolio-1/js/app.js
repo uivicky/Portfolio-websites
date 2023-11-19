81e768fe7b57ca252fbbf20a0fb35c8d67ce9daa
Vicky <ui.vicky2002@gmail.com>
2023-11-19 11:03:32 +0530

    Successful commit

---
+++ Portfolio-1/js/app.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+});
+
+
+sr.reveal('.left', {delay: 550, origin: 'left'});
+sr.reveal('.right', {delay: 500, origin: 'right'});
+sr.reveal('.leftSection', {delay: 600, origin: 'left'});
+sr.reveal('.rightSection img', {delay: 500, origin: 'right'});
